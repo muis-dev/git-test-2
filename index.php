afaf032d23f8a17412da2b75e8f3e34252ff3d28
muis <muis.muhtadi.mail@gmail.com>
2021-06-26 09:32:55 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git</title>
 </head>
+
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptas iure, asperiores porro, ullam officiis, odio ipsam cum alias voluptatum veniam praesentium maiores quasi reprehenderit ea eum exercitationem saepe commodi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
+
 </html>
